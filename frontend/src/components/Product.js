3b0e27af39bb8dc92ed8070cd3605dea6ecbfff7
Deena Smith <deenasmith1630@gmail.com>
2022-07-18 16:37:01 -0500

    ratings page added, stars display for each product based on rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 
 const Product = ({ product }) => {
@@ -18,9 +19,7 @@ const Product = ({ product }) => {
                 </a>
 
                 <Card.Text as='div'>
-                    <div className="my-3">
-                        {product.rating} from {product.numReviews} Reviews
-                    </div>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                 </Card.Text>
                 <Card.Text as='h3'>${product.price}</Card.Text>
             </Card.Body>
