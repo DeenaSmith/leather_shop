13317e7b774d3844ce9ad9cc79107b927e603979
Deena Smith <deenasmith1630@gmail.com>
2022-07-18 18:10:38 -0500

    routes added, cards styled

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
     return (
@@ -54,8 +55,15 @@ const Rating = ({ value, text, color }) => {
     )
 }
 
+
 Rating.defaultProps = {
     color: '#fdda18',
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
 
